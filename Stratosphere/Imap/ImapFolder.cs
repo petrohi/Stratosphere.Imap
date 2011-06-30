d58918989b19fbb6f774b572bc3a911352c91671
Tyler Austen <TylerAusten@gmail.com>
2011-06-29 17:25:38 -0700

    Expose UIDVALIDITY value from ImapFolder

--- Stratosphere/Imap/ImapFolder.cs
+++ Stratosphere/Imap/ImapFolder.cs
@@ -35,16 +35,17 @@ namespace Stratosphere.Imap
                         }
                     }
 
-                    var uidNextPos = list.IndexOfString("[UIDNEXT");
-
-                    if (uidNextPos >= 0)
+                    long temp = 0;
+                    if (TryGetBracketedLongValue(list, "[UIDNEXT", out temp))
                     {
-                        long uidNext = long.MaxValue;
-                        if (long.TryParse(list.GetStringAt(uidNextPos + 1).TrimEnd(']'), out uidNext))
-                        {
-                            UidNext = uidNext;
-                        }
+                        UidNext = temp;
                     }
+
+                    if (TryGetBracketedLongValue(list, "[UIDVALIDITY", out temp))
+                    {
+                        UidValidity = temp;
+                    }
+
                 }
                 else
                 {
@@ -53,10 +54,25 @@ namespace Stratosphere.Imap
             }
         }
 
+        private bool TryGetBracketedLongValue(ImapList list, string name, out long retVal)
+        {
+            bool isOk = false;
+            retVal = default(long);
+
+            var pos = list.IndexOfString(name);
+            if (pos >= 0)
+            {
+                isOk = long.TryParse(list.GetStringAt(pos + 1).TrimEnd(']'), out retVal);
+            }
+
+            return isOk;
+        }
+
         public string Name { private set; get; }
         public bool IsReadOnly { private set; get; }
         public int ExistsCount { private set; get; }
         public int RecentCount { private set; get; }
         public long UidNext { private set; get; }
+        public long UidValidity { private set; get; }
     }
 }
